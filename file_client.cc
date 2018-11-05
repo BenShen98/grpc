422f8f951007fd9a1739736e654cd9ffdbc1be55
leyang shen <shenleyang007@gmail.com>
2018-11-05 10:20:06 +0000

    clean up

--- file_client.cc
+++ file_client.cc
@@ -1,37 +1,12 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 #include <iostream>
-#include <memory>
 #include <string>
 #include <fstream>
-#include <thread>
-#include <chrono>
-#include <bitset>
-
 
 #include <grpcpp/grpcpp.h>
 
 #include "file.grpc.pb.h"
 
-
 #define defaultbuffersize 4096 // suggested size by https://stackoverflow.com/questions/3033771/file-i-o-with-streams-best-memory-buffer-size
-
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -51,6 +26,7 @@ class FileClient {
     ClientContext context;
     char * memblock;
 
+    //open file and prepare meta data
     int fileByte;
     std::ifstream f (filepath, std::ios::in|std::ios::binary|std::ios::ate);
     if (f.is_open()){
@@ -65,42 +41,32 @@ class FileClient {
     context.AddMetadata("f_buffer",std::to_string(buffersize));
 
 
-    // stream start;
+    // file stream start
     memblock = new char [buffersize];
     f.seekg (0, std::ios::beg);
     std::unique_ptr<ClientWriter<FileRequest> > writer(stub_->UploadFile(&context, &reply));
-    // char x[5]={0x48,0x65,0x6c,0x6c,0x6f};
-    // std::string y="! Ben!";
     int size=buffersize;
     while (f){
-        // std::cout << "enter loop" << '\b';
         f.read(memblock, buffersize);
-        // for(int i=0;i<buffersize;i++){
-        //   std::cout << std::bitset<8>(memblock[i]) << '\t';
-        // }
-        // std::cout << '\n';
         if(f.eof()){
-          //adjust file size for the last steam, may not use all buffer
+          //adjust "content" size for the last steam, it may be smaller than the buffer size
           size=fileByte%buffersize;
         }
         FileRequest request;
-        //NEED SIZE, AS STRING WILL CONTAIN 0X00 (NULL), CAUSE CORRUPTED FILE
+        //NEED SIZE. Without size, if char* contain 0x00, it will be treated as EOF, cause incomplete byte transfer
         std::string s(memblock, size);
         request.set_content(s);
         if(!writer->Write(request)){
           break;
         }
-
-        // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
     delete[] memblock;
 
     //stream done
-
-
     writer->WritesDone();
     Status status = writer->Finish();
 
+    //waiting server to reply
     if (status.ok()) {
       std::cout << "client:\t\t replay message: "<<reply.message() << '\n';
     }else{
@@ -114,21 +80,27 @@ class FileClient {
 };
 
 int main(int argc, char** argv) {
+
+  // --help
   if(argc<4 || argc>5 ){
     std::cout << "client:\t\t missing arguement.\n------ need file path, string, number, [optional buffersize]" << '\n';
     return -1;
   }
 
-  FileClient file(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
-
+  //get arguement
   std::string path=argv[1];
   std::string str=argv[2];
   long num=std::stol(argv[3]);
 
+  //connect to server for upload
+  FileClient file(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
   if(argc==4){
+    //when arg does not conatin buffer size
     file.UploadFile(path,str,num,defaultbuffersize);
   }else{
+    //when arg conatin buffer size
     file.UploadFile(path,str,num,std::stoi(argv[4]));
   }
+
   return 0;
 }
--- file_server.cc
+++ file_server.cc
@@ -1,24 +1,4 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 #include <iostream>
-#include <memory>
-#include <bitset>
 #include <string>
 #include <map>
 #include <fstream>
@@ -37,20 +17,13 @@ using file::FileRequest;
 using file::FileReply;
 using file::File;
 
-// Logic and data behind the server's behavior.
 class FileServiceImpl final : public File::Service {
-  // Status PerUploadFile(ServerContext* context, const PerFileRequest* request, Token* token) override {
-  //   int hash=PerFileHash(request->filebyte(),request->num(),request->str());
-  //   std::cout << request->filebyte()<<"  |  "<<request->num()<<"  |  "<<request->str() << '\n';
-  //   token->set_hash(hash);
-  //   return Status::OK;
-  // }
 
   Status UploadFile(::grpc::ServerContext* context, ::grpc::ServerReader< ::file::FileRequest>* reader, ::file::FileReply* response) override{
+
     //getting context
     std::string f_str,f_numstr,f_bytestr,f_bufferSizestr;
     auto cc=context->client_metadata();
-
     if(getVal(cc,"f_str",f_str) && getVal(cc,"f_num",f_numstr) && getVal(cc,"f_byte",f_bytestr)  && getVal(cc,"f_buffer",f_bufferSizestr) ){
         std::cout<< "server:\t\t serving "<< f_str<<", with id "<<f_numstr<<". Transferred file size "<<f_bytestr <<", with buffersize "<< f_bufferSizestr << '\n';
     }else{
@@ -59,7 +32,7 @@ class FileServiceImpl final : public File::Service {
       return Status(StatusCode::INVALID_ARGUMENT, "Need str, num and file size to upload file\n");
     }
 
-    //can be used in application
+    //can be used in later application
     // long f_num;
     // int f_byte,f_bufferSize;
     // f_bufferSize=std::stoi(f_bufferSizestr);
@@ -69,30 +42,11 @@ class FileServiceImpl final : public File::Service {
 
     //read stream
     std::ofstream outfile ("server/"+f_numstr+"."+f_str,std::ifstream::binary);
-    // char* buffer = new char[f_buffer];
-
     FileRequest file;
     while (reader->Read(&file)) {
-      // reader->Read(&file);
       const char *charContent=file.content().c_str();
-      // std::cout << "str length"<<file.content().length()<<", buffer length"<<f_bufferSize << '\n';
-      // for (int i=0;i<f_bufferSize;i++){
-      //   std::cout << std::bitset<8>(charContent[i]) << '\t';
-      // }
-      // std::cout << '\n';
-      // int len=f_bufferSize;
-      // if(f_byteRemain-f_bufferSize>0){
-      //   //keeping count of size of bit remaining
-      //   f_byteRemain=f_byteRemain-f_bufferSize;
-      // }else{
-      //   //reached end of file, trim the extra bit
-      //   len=f_byteRemain;
-      //   f_byteRemain=f_byteRemain-f_bufferSize;
-      // }
-
       outfile.write(charContent,file.content().length());
     }
-    // delete[] buffer;
     outfile.close();
 
     //stream done
@@ -120,19 +74,11 @@ private:
 void RunServer() {
   std::string server_address("0.0.0.0:50051");
   FileServiceImpl service;
-
   ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
   builder.RegisterService(&service);
-  // Finally assemble the server.
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "server:\t\t Server listening on " << server_address << std::endl;
-
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
   server->Wait();
 }
 
